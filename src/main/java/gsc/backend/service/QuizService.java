cc5ef494a68bcc9b114ed6adde70363a1770a591
alsrudursla <nuy0307@naver.com>
2024-02-09 19:04:30 +0900

    chore: quiz response 수정

--- src/main/java/gsc/backend/service/QuizService.java
+++ src/main/java/gsc/backend/service/QuizService.java
@@ -6,6 +6,7 @@ import gsc.backend.domain.QuizAnswer;
 import gsc.backend.domain.enums.QuizType;
 import gsc.backend.dto.response.QuizDataDTO;
 import gsc.backend.dto.response.QuizOptionsDTO;
+import gsc.backend.dto.response.QuizResponseDTO;
 import gsc.backend.repository.ChoiceRepository;
 import gsc.backend.repository.EducationRepository;
 import gsc.backend.repository.QuizAnswerRepository;
@@ -26,7 +27,7 @@ public class QuizService {
     private final QuizAnswerRepository quizAnswerRepository;
     private final ChoiceRepository choiceRepository;
 
-    public QuizDataDTO getQuiz(Long quizId) {
+    public QuizResponseDTO getQuiz(Long quizId) {
 
         // 퀴즈
         Quiz quiz = quizRepository.findById(quizId).orElseThrow(RuntimeException::new);
@@ -51,14 +52,13 @@ public class QuizService {
                 .options(optionsDTO)
                 .build();
 
-        return quizDataDTO;
-    }
+        // 반환값 세팅
+        QuizResponseDTO quizResponseDTO = QuizResponseDTO.builder()
+                .id(quizId)
+                .type(quiz.getQuizType())
+                .item(quizDataDTO)
+                .build();
 
-    public QuizType getQuizType(Long quizId) {
-
-        // 퀴즈
-        Quiz quiz = quizRepository.findById(quizId).orElseThrow(RuntimeException::new);
-
-        return quiz.getQuizType();
+        return quizResponseDTO;
     }
 }
